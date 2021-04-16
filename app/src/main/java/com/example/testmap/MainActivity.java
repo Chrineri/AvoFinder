6f4b386f3512569d922da27f75272d1433b53e0f
Christian <nerichristian4@gmail.com>
2021-04-16 21:44:21 +0200

    Seconda modifica

--- app/src/main/java/com/example/testmap/MainActivity.java
+++ app/src/main/java/com/example/testmap/MainActivity.java
@@ -5,12 +5,14 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -25,6 +27,7 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Polygon;
 
 import java.io.IOException;
@@ -38,9 +41,14 @@ public class MainActivity extends AppCompatActivity{
     private MapView map = null;
     private List l;
     private List<Polygon> p = null;
+    private int piano = 0;
+    List<Marker> markers = new ArrayList<>();
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
 
 
@@ -81,7 +89,7 @@ public class MainActivity extends AppCompatActivity{
 
         Drawable defaultMarker = getResources().getDrawable(R.drawable.marker_default);
         Bitmap defaultBitmap = ((BitmapDrawable) defaultMarker).getBitmap();
-        Style defaultStyle = new Style(defaultBitmap, 0x901010AA, 5f, Color.CYAN);
+        Style defaultStyle = new Style(defaultBitmap, Color.parseColor("#104281"), 5f, Color.parseColor("#f1c047"));
         FolderOverlay geoJsonOverlay = (FolderOverlay) kmlDocument.mKmlRoot.buildOverlay(map, defaultStyle, null, kmlDocument);
 
         l = geoJsonOverlay.getItems();
@@ -90,20 +98,57 @@ public class MainActivity extends AppCompatActivity{
         for(int i=0;i<l.size();i++) {
             p = (List<Polygon>) l;
         }
-        for(int i=0;i<p.size();i++) {
-
-            if(p.get(i).getSubDescription().contains("level=0")) {
-                p.get(i).setEnabled(true);
-                map.invalidate();
-            }else {
-                p.get(i).setEnabled(false);
-            }
-        }
 
 
-        Button invio = (Button) findViewById(R.id.l1);
-        invio.setOnClickListener(new View.OnClickListener() {
+        Button p0 = (Button) findViewById(R.id.l0);
+        Button p1 = (Button) findViewById(R.id.l1);
+        Button p2 = (Button) findViewById(R.id.l2);
+        Button p3 = (Button) findViewById(R.id.l3);
+        Button p4 = (Button) findViewById(R.id.l4);
+        CheckBox c = (CheckBox) findViewById(R.id.check);
+        p0.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.gray));
+        p0.setTextColor(Color.WHITE);
+
+        p0.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                setDefaultColor(p0,p1,p2,p3,p4);
+                p0.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.gray));
+                p0.setTextColor(Color.WHITE);
+                piano = 0;
+                if(c.isChecked()){
+                    setMarker();
+                }
+
+
+                for(int i=0;i<p.size();i++) {
+
+                    if(p.get(i).getSubDescription().contains("level=0")) {
+                        p.get(i).setEnabled(true);
+                        map.invalidate();
+                    }else {
+                        p.get(i).setEnabled(false);
+                    }
+            /*if(p.get(i).getTitle().equals("4-13"))
+                p.get(i).getFillPaint().setColor(Color.BLUE);//Cambiare i colori ai singoli poligoni
+            else
+                p.get(i).getFillPaint().setColor(Color.GREEN);*/
+                }
+            }
+        });
+
+
+        p1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                setDefaultColor(p0,p1,p2,p3,p4);
+                p1.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.gray));
+                p1.setTextColor(Color.WHITE);
+                piano = 1;
+
+                if(c.isChecked()){
+                    setMarker();
+                }
+
                 for(int i=0;i<p.size();i++) {
 
                     if(p.get(i).getSubDescription().contains("level=1")) {
@@ -120,7 +165,122 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
+        p2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setDefaultColor(p0,p1,p2,p3,p4);
+                p2.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.gray));
+                p2.setTextColor(Color.WHITE);
+                piano = 2;
+
+                if(c.isChecked()){
+                    setMarker();
+                }
+
+                for(int i=0;i<p.size();i++) {
+
+                    if(p.get(i).getSubDescription().contains("level=2")) {
+                        p.get(i).setEnabled(true);
+                        map.invalidate();
+                    }else {
+                        p.get(i).setEnabled(false);
+                    }
+            /*if(p.get(i).getTitle().equals("4-13"))
+                p.get(i).getFillPaint().setColor(Color.BLUE);//Cambiare i colori ai singoli poligoni
+            else
+                p.get(i).getFillPaint().setColor(Color.GREEN);*/
+                }
+            }
+        });
+
+        p3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setDefaultColor(p0,p1,p2,p3,p4);
+                p3.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.gray));
+                p3.setTextColor(Color.WHITE);
+                piano = 3;
+
+                if(c.isChecked()){
+                    setMarker();
+                }
+
+                for(int i=0;i<p.size();i++) {
+
+                    if(p.get(i).getSubDescription().contains("level=3")) {
+                        p.get(i).setEnabled(true);
+                        map.invalidate();
+                    }else {
+                        p.get(i).setEnabled(false);
+                    }
+            /*if(p.get(i).getTitle().equals("4-13"))
+                p.get(i).getFillPaint().setColor(Color.BLUE);//Cambiare i colori ai singoli poligoni
+            else
+                p.get(i).getFillPaint().setColor(Color.GREEN);*/
+                }
+            }
+        });
+
+        p4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setDefaultColor(p0,p1,p2,p3,p4);
+                p4.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.gray));
+                p4.setTextColor(Color.WHITE);
+                piano = 4;
+
+                if(c.isChecked()){
+                    setMarker();
+                }
+
+                for(int i=0;i<p.size();i++) {
+
+                    if(p.get(i).getSubDescription().contains("level=4")) {
+                        p.get(i).setEnabled(true);
+                        map.invalidate();
+                    }else {
+                        p.get(i).setEnabled(false);
+                    }
+            /*if(p.get(i).getTitle().equals("4-13"))
+                p.get(i).getFillPaint().setColor(Color.BLUE);//Cambiare i colori ai singoli poligoni
+            else
+                p.get(i).getFillPaint().setColor(Color.GREEN);*/
+                }
+            }
+        });
+
+
+        c.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if(c.isChecked()){
+                        for(int i=0;i<p.size();i++) {
+                        if(p.get(i).getSubDescription().contains("level="+piano)) {
+                            if(p.get(i).getTitle()!=null) {
+                                p.get(i).setEnabled(true);
+
+                                Marker m = new Marker(map);
+                                m.setTextLabelBackgroundColor(Color.TRANSPARENT);
+                                m.setTextLabelForegroundColor(Color.BLACK);
+                                m.setTextIcon(p.get(i).getTitle());
+                                m.setPosition(p.get(i).getInfoWindowLocation());
+                                map.getOverlayManager().add(m);
+                                markers.add(m);
+                                map.invalidate();
+                            }
+                        }else {
+                            p.get(i).setEnabled(false);
+                        }
+                    }
+                }else{
+                    for(int i=0;i<markers.size();i++)
+                        map.getOverlayManager().remove(markers.get(i));
+                        map.invalidate();
+                }
+
+            }
+        });
+
+
         map.getOverlays().add(geoJsonOverlay);
+
         map.setHorizontalMapRepetitionEnabled(false);
         map.setVerticalMapRepetitionEnabled(false);
         IMapController mapController = map.getController();
@@ -129,13 +289,37 @@ public class MainActivity extends AppCompatActivity{
         mapController.setCenter(startPoint);
         map.setMinZoomLevel((double) 18);
         map.setScrollableAreaLimitLatitude(45.07146656030419, 45.06952171642693,10);
-        map.setScrollableAreaLimitLongitude(7.691488068154647,7.693766902971969,10);
+        map.setScrollableAreaLimitLongitude(7.69138,7.693766902971969,10);
         requestPermissionsIfNecessary(new String[] {
 
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
         map.invalidate();
 
+    }
+
+    public void setMarker() {
+        for(int i=0;i<markers.size();i++)
+            map.getOverlayManager().remove(markers.get(i));
+            map.invalidate();
+        for (int i = 0; i < p.size(); i++) {
+            if (p.get(i).getSubDescription().contains("level=" + piano)) {
+                if (p.get(i).getTitle() != null) {
+                    p.get(i).setEnabled(true);
+
+                    Marker m = new Marker(map);
+                    m.setTextLabelBackgroundColor(Color.TRANSPARENT);
+                    m.setTextLabelForegroundColor(Color.BLACK);
+                    m.setTextIcon(p.get(i).getTitle());
+                    m.setPosition(p.get(i).getInfoWindowLocation());
+                    map.getOverlayManager().add(m);
+                    markers.add(m);
+                    map.invalidate();
+                }
+            } else {
+                p.get(i).setEnabled(false);
+            }
+        }
     }
 
     @Override
@@ -184,5 +368,18 @@ public class MainActivity extends AppCompatActivity{
                     permissionsToRequest.toArray(new String[0]),
                     REQUEST_PERMISSIONS_REQUEST_CODE);
         }
+    }
+
+    private void setDefaultColor(Button b1,Button b2,Button b3,Button b4,Button b5){
+        b1.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.yellow));
+        b1.setTextColor(Color.parseColor("#104281"));
+        b2.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.yellow));
+        b2.setTextColor(Color.parseColor("#104281"));
+        b3.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.yellow));
+        b3.setTextColor(Color.parseColor("#104281"));
+        b4.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.yellow));
+        b4.setTextColor(Color.parseColor("#104281"));
+        b5.setBackgroundTintList(getApplicationContext().getResources().getColorStateList(R.color.yellow));
+        b5.setTextColor(Color.parseColor("#104281"));
     }
 }
